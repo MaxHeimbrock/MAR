aad11eeb53310a969452a8d0ad56132827bf5921
Max <max.heimbrock@gmx.de>
2019-01-20 19:36:32 +0100

    First Commit - Sunday with Reza and Max

---
+++ Assets/ReadFromString.cs
@@ -0,0 +1,26 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadFromString : MonoBehaviour {
+
+    public bool read = false;
+    TextToSpeech textToSpeech;
+
+    void Start()
+    {
+        textToSpeech = gameObject.GetComponent<TextToSpeech>();
+        textToSpeech.Voice = TextToSpeechVoice.Mark;
+        //textToSpeech.SpeakText("Welcome to the Holographic App ! You can use Gaze, Gesture and Voice Command to interact with it!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(read)
+        {
+            textToSpeech.SpeakText("yoloooooo");
+        }
+    }
+}
---
+++ Assets/TrackingManager.cs
@@ -0,0 +1,29 @@
+﻿using HoloToolkit.Unity.SpatialMapping;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TrackingManager : MonoBehaviour
+{
+    public bool gefunden = false;
+    public GameObject pathFinding;
+    
+    TextMesh textMesh;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {        
+
+
+        if (gefunden)
+            pathFinding.SetActive(true);
+
+
+    }
+}
