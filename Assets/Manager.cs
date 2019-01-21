1cda3466cb8e3310ee52720fc49532301c6d9097
MaxHeimbrock <max.heimbrock@gmx.de>
2019-01-21 10:48:12 +0100

    Revert "Revert "First Commit - Sunday with Reza and Max""
    This reverts commit 36a41b5c5ff7750f53403a41519bbc466cf23d79.

---
+++ Assets/Manager.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+
+    public GameObject billboard;
+    enum State { BeforePathSet, BeforeProduct, LookingForProduct};
+    State currentState;
+    AudioSource audioSource;
+
+    public bool gefunden = false;
+    public bool arrivedAtDestination;
+
+    public GameObject pathFindingMax;
+    public GameObject pathFindingOther;
+
+    private bool commandIsGiven = false;
+    public VoiceCommand voiceCommand;
+
+    int posterFounded = 0;
+    public AudioClip whichSectionAudioFile;
+    public AudioClip whichProductAudioFile;
+
+    public GameObject cornyProduct;
+    bool playProductSound = false;
+    // Use this for initialization
+    void Start () {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        currentState = State.BeforePathSet;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (currentState == State.BeforePathSet)
+        {
+            if (!commandIsGiven && gefunden)
+            {
+                AskForInput();
+            }
+            if (commandIsGiven && voiceCommand.pathIsSet)
+            {
+                if (voiceCommand.pathIsMax)
+                {
+                    pathFindingMax.SetActive(true);
+                }
+                else
+                {
+                    pathFindingOther.SetActive(true);
+                }
+                GoToNextState();
+                commandIsGiven = false;
+            }
+
+            
+        }
+
+        if (currentState == State.BeforeProduct)
+        {
+            if (voiceCommand.pathIsMax)
+            {
+                arrivedAtDestination = pathFindingMax.GetComponent<Pathfinding>().arrivedAtDestination;
+            }
+            else
+            {
+                arrivedAtDestination = pathFindingOther.GetComponent<Pathfinding>().arrivedAtDestination;
+            }
+
+            if (!commandIsGiven && arrivedAtDestination)
+            {
+                AskForInput();
+            }
+            if (commandIsGiven && voiceCommand.prouctIsSet)
+            {
+                if (voiceCommand.productIsMilk)
+                {
+                    billboard.GetComponent<TextMesh>().text = "From Manager: Milk";
+                    GoToNextState();
+                }
+                else
+                {
+                    billboard.GetComponent<TextMesh>().text = "From Manager: Corny";
+                    //cornyProduct.SetActive(true);
+                    playProductSound = true;
+                    GoToNextState();
+                    
+                }
+            }
+        }
+
+        if (playProductSound)
+        {
+            billboard.GetComponent<TextMesh>().text = "Play for Corny";
+            cornyProduct.GetComponent<AudioSource>().Play(0);
+        }
+
+    }
+
+    void AskForInput()
+    {
+        commandIsGiven = true;
+        if (currentState == State.BeforePathSet)
+        {
+            audioSource.clip = whichSectionAudioFile;
+            audioSource.Play(0);
+        }
+        else if (currentState == State.BeforeProduct)
+        {
+            audioSource.clip = whichProductAudioFile;
+            audioSource.Play(0);
+        }
+    }
+
+    void SelectPath()
+    {
+
+    }
+
+
+    void GoToNextState()
+    {
+        if(currentState == State.BeforePathSet)
+        {
+            currentState = State.BeforeProduct;
+        }
+        else if (currentState == State.BeforeProduct)
+        {
+            currentState = State.LookingForProduct;
+        }
+    }
+
+}
---
+++ Assets/VoiceCommand.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceCommand : MonoBehaviour
+{
+
+    public GameObject billboard;
+    Pathfinding pathfinding;
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    public bool pathIsSet = false;
+    public bool pathIsMax = false;
+    public bool prouctIsSet = false;
+    public bool productIsMilk = false;
+    //public GameObject drinkSection;
+    //public GameObject foodSection;
+
+    private void Awake()
+    {
+        //pathfinding = GetComponent<Pathfinding>();
+    }
+
+    void Start()
+    {
+        actions.Add("Max", SetMaxAsPath);
+        actions.Add("Other", SetOtherAsPath);
+        actions.Add("milk", SetMilkAsProduct);
+        actions.Add("corny", SetCornyAsProduct);
+        
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizeSpeech;
+        keywordRecognizer.Start();
+    }
+
+    void RecognizeSpeech(PhraseRecognizedEventArgs speech)
+    {
+        print(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    void SetMaxAsPath()
+    {
+        billboard.GetComponent<TextMesh>().text = "Max";
+        pathIsSet = true;
+        pathIsMax = true;
+    }
+    void SetOtherAsPath()
+    {
+        billboard.GetComponent<TextMesh>().text = "Other";
+        pathIsSet = true;
+    }
+
+    void SetMilkAsProduct()
+    {
+        billboard.GetComponent<TextMesh>().text = "Milk";
+        prouctIsSet = true;
+        productIsMilk = true;
+    }
+
+    void SetCornyAsProduct()
+    {
+        billboard.GetComponent<TextMesh>().text = "Corny";
+        prouctIsSet = true;
+    }
+
+
+}
